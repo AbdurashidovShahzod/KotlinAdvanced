8bd561b17d829723c49d3ea40d54e40a0765cdce
Shahzod <shahzod9933@gmail.com>
2022-02-03 15:56:12 +0500

    Update Main.kt

--- src/designpatterns/mediatorpattern/Main.kt
+++ src/designpatterns/mediatorpattern/Main.kt
@@ -1,12 +1,7 @@
 package designpatterns.mediatorpattern
 
 fun main(args: Array<String>) {
-    /**
-     * Albatta design patternlarni o'rganish ancha qiyin bo'layabdi
-     * shuning uchun ham internet orqali har xil manbalardan foydalangan
-     * holda code larni qayta qayta yozib har bitta jarayoni nima bo'layotganini
-     * tushunishga harakat qildim va bunga oz bo'lsada natijaga erishdim deb o'ylayman...
-     */
+   
     val atcMediator = ATCMediator()
     val sparrow101 = Flight(atcMediator)
     val mainRunway = Runway(atcMediator)
