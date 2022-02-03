60471927332402249ced4fb0941d6ab4b532cf02
Shahzod <shahzod9933@gmail.com>
2022-02-03 15:57:04 +0500

    Update Main.kt

--- src/designpatterns/statepattern/Main.kt
+++ src/designpatterns/statepattern/Main.kt
@@ -2,12 +2,6 @@ package designpatterns.statepattern
 
 fun main(args: Array<String>) {
 
-    /**
-     * Albatta design patternlarni o'rganish ancha qiyin bo'layabdi
-     * shuning uchun ham internet orqali har xil manbalardan foydalangan
-     * holda code larni qayta qayta yozib har bitta jarayoni nima bo'layotganini
-     * tushunishga harakat qildim va bunga oz bo'lsada natijaga erishdim deb o'ylayman...
-     */
     val statateContext = AlerStatateContext()
     statateContext.setState(VibratsiyaJarayoni())
     statateContext.alert()
